93d8786b02499e8d12b9ed64461783fff6146dfd
cobyeast <cobyeastwood@gmail.com>
2020-11-06 14:03:20 -0800

    improved anagrams filtering and sorting

--- src/anagram.rs
+++ src/anagram.rs
@@ -6,44 +6,54 @@ use std::collections::HashSet;
 
 pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
   let mut word_set: HashSet<char> = HashSet::<char>::new();
+  let num = word.chars().count() as i32;
 
-  for i in word.chars() {
-    if let mut x = i.to_lowercase() {
-      match x.next() {
+  let mut chars_one = String::new();
+  let mut chars_two = Vec::<String>::new();
+
+  // Convert word to lowercase and add onto new HashSet
+  for chars in word.chars() {
+    match chars.to_lowercase().next() {
+      Some(c) => {
+        word_set.insert(c);
+        chars_one.push(c)
+      }
+      None => (),
+    }
+  }
+
+  let mut chars_num: Vec<Vec<i32>> = Vec::new();
+
+  for p in possible_anagrams {
+    let mut res = Vec::<i32>::new();
+    let mut ana = String::new();
+    // Convert anagrams to lowercase and perform match check
+    for chars in p.chars() {
+      match chars.to_lowercase().next() {
         Some(c) => {
-          word_set.insert(c);
+          // See if provided anagram char exists in word HashSet
+          if word_set.contains(&c) {
+            res.push(1);
+          } else {
+            res.push(0)
+          }
+          ana.push(c);
         }
         None => (),
       }
     }
+    chars_num.push(res);
+    chars_two.push(ana);
   }
 
-  let mut h: Vec<usize> = Vec::<usize>::new();
+  let mut set = HashSet::<&str>::new();
 
-  for p in possible_anagrams {
-    let mut f: Vec<i32> = Vec::<i32>::new();
-    for c in p.chars() {
-      if let mut x = c.to_lowercase() {
-        match x.next() {
-          Some(c) => {
-            if word_set.contains(&c) {
-              f.push(1);
-            }
-          }
-          None => (),
-        }
-      }
-    }
-    h.push(f.len());
-  }
-
-  let mut map: HashSet<&'a str> = HashSet::<&'a str>::new();
-
-  for (i, w) in h.iter().enumerate() {
-    if w == &word.chars().count() && possible_anagrams[i] != word {
-      map.insert(possible_anagrams[i]);
+  for (i, n) in chars_num.iter().enumerate() {
+    if n.iter().sum::<i32>() == num && n.len() as i32 == num && chars_one != chars_two[i] {
+      // Push borrowed anagram
+      set.insert(possible_anagrams[i]);
     }
   }
 
-  map
+  set
 }
--- src/fibonacii.rs
+++ src/fibonacii.rs
@@ -1,11 +1,11 @@
 // Fibonacii function generates first 100 numbers in squence
 pub fn fibonacii() -> Vec<u128> {
-  // u128 holds digits until 340282366920938463463374607431768211455
-  let mut fib: Vec<u128> = vec![0, 1];
+    // u128 holds digits until 340282366920938463463374607431768211455
+    let mut fib: Vec<u128> = vec![0, 1];
 
-  for i in 2..100 {
-    fib.push(fib[i - 1] + fib[i - 2]);
-  }
+    for i in 2..100 {
+        fib.push(fib[i - 1] + fib[i - 2]);
+    }
 
-  return fib;
+    return fib;
 }
